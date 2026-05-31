470397ac4f15fdef40c41b1311a6b7c1b93306ac
root <root@DESKTOP-P2Q7RB4.localdomain>
2026-05-31 15:54:26 +0900

    管理画面の検索機能を実装

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,12 +8,36 @@ use App\Models\Category;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $contacts = Contact::paginate(7);
+        $contacts = Contact::query();
+
+        if ($request->keyword) {
+            $contacts->where(function ($query) use ($request) {
+                $query->where('first_name', 'like', '%' . $request->keyword . '%')
+                    ->orWhere('last_name', 'like', '%' . $request->keyword . '%')
+                    ->orWhere('email', 'like', '%' . $request->keyword . '%');
+            });
+        }
+
+        if ($request->gender && $request->gender != 0) {
+            $contacts->where('gender', $request->gender);
+        }
+
+        if ($request->category_id) {
+            $contacts->where('category_id', $request->category_id);
+        }
+
+        if ($request->date) {
+            $contacts->whereDate('created_at', $request->date);
+        }
+
+        $contacts = $contacts->paginate(7);
+
         $categories = Category::all();
 
         return view('admin.index', compact('contacts', 'categories'));
+
     }
 
     public function show(Contact $contact)
